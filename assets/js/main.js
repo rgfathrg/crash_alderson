3f37dbb56e8d7591dff70e2e57a6285faef32dd2
JPGAGHAN <jpgaghan@gmail.com>
2018-08-11 13:41:04 -0400

    maps

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=Fst7jzMSw05CNr3UdA1wrZAywnNi0A3j";
+//     var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=Fst7jzMSw05CNr3UdA1wrZAywnNi0A3j";
     
     
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        // async:true,
-        // dataType: "json",
-        }).then(function(response) {;
-            console.log(response)
-            console.log(response._embedded.events);
-        });
-    });
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//         // async:true,
+//         // dataType: "json",
+//         }).then(function(response) {;
+//             console.log(response)
+//             console.log(response._embedded.events);
+//         });
+//     });
