9d7a460ac863441f4f92d903bc328941690e9c1e
Ghost <mkp0203@live.com>
2018-08-18 11:12:40 -0400

    consolidation of all code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
 
-    // Refresh Page
+    // Refresh the page when clicking on brand in the navbar
     $("#brand").click(function () {
         document.location.reload(true);
     });
+
+    var uniCity = [];
+    var limit = 25;
 
     // GO Button
     $("#goBtn").on("click", function () {
 
         // Variables
-        var cities = ["charlotte", "houston", "san+diego", "new+york", "san+francisco", "orlando", "charleston", "boston", "miami", "tampa", "chicago", "buffalo", "baltimore", "columbus", "cleveland"];
+        var cities = ["charlotte", "houston", "san+diego", "new+york", "san+francisco", "orlando", "charleston", "boston", "miami", "tampa", "chicago", "buffalo", "baltimore", "columbus", "cleveland", "dublin"];
         var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Fst7jzMSw05CNr3UdA1wrZAywnNi0A3j";
         var startDate = "2018-09-01T01:00:00Z";
         var endDate = "2018-10-30T21:59:00Z";
@@ -24,7 +27,7 @@ $(document).ready(function () {
 
         // Adds the randomely selected city to the City Banner after user clicks GO!
         var banner = $("<div>").html("<p>Your city: " + city + "!</p>");
-        banner.addClass("card bg-light text-dark banner-glow-dark").attr("id", "bannerMargin");
+        banner.addClass("card bg-light text-dark banner-glow-dark").attr("id", "cityBanner");
         $("#cityBanner").html(banner);
 
         // Centers/zooms the map on the randomly selected city
@@ -43,66 +46,91 @@ $(document).ready(function () {
                 city: city,
                 startDateTime: startDate,
                 endDateTime: endDate,
+                size: limit,
+                locale: "en"
             }
         }).then(function (response) {
 
             $("#events").empty();
-            // console.log(response)
-            // console.log(response._embedded.events);
-            // console.log(response._embedded.events[0].images);
-
-            var events = response._embedded.events;
-
-            // Adds initial event row
-            var tr = $("<div>").addClass("row");
-            tr.addClass("cardRow");
-            $("#events").append(tr);
+            // console.log(response);
 
             console.log(response._embedded.events);
             var events = response._embedded.events;
 
             // Loops through the events and adds them to the event rows
             for (var i = 0; i < events.length; i++) {
-                var card = $("<div>").addClass("card float-left border cards col-2");
-                var cardBody = $("<div>").attr("class", "card-body");
-                var eventDates = events[i].dates.start.localDate;
-                var eventTime = events[i].dates.start.localTime;
-                var eventPics = events[i].images[0].url;
-                var eventTitle = events[i].name;
-                var p = $("<p>");
-                p.html(eventTitle + "<br>" + eventDates + "<br>" + eventTime);
-                var img = $("<img>").attr("class", "card-img-top");
-                img.attr("src", eventPics);
-                cardBody.append(p);
-                card.append(img);
-                card.append(cardBody);
-                tr.append(card);
 
-                // Conditional for dynamically creating rows
-                // If a 5th card tries to enter an event row
-                // it is pushed to the next row
-                if ((i + 1) % 4 === 0 && (i + 1) !== events.length) {
-                    tr = $("<div>").addClass("row");
-                    tr.addClass("cardRow");
-                    $("#events").append(tr);
-                };
+                var eventTitle = events[i].name;
+                var id = events[i].id;
+                uniCity[eventTitle] = id;
+                
             };
+        });
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            data: {
+                city: city,
+                startDateTime: startDate,
+                endDateTime: endDate,
+                size: limit,
+                locale: "en"
+            }
+        }).then(function (response) {
+            console.log(response);
+
+            // Adds initial event row
+            var tr = $("<div>").addClass("row");
+            tr.addClass("cardRow");
+            $("#events").append(tr);
+
+
+            for (var key in uniCity) {
+                uniqueId = uniCity[key];
+
+                for (i = 0; i < limit; i++) {
+                    if (response._embedded.events[i].id === uniqueId) {
+
+                        var card = $("<div>").addClass("card cards col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-3");
+                        var cardBody = $("<div>").attr("class", "card-body");
+                        var eventDates = response._embedded.events[i].dates.start.localDate;
+                        var eventTime = response._embedded.events[i].dates.start.localTime;
+                        var eventPics = response._embedded.events[i].images[0].url;
+                        var eventTitle = response._embedded.events[i].name;
+
+                        var p = $("<p>");
+                        p.html(eventTitle + "<br>" + eventDates + "<br>" + eventTime);
+
+                        var img = $("<img>").attr("class", "card-img-top");
+                        img.attr("src", eventPics);
+
+                        cardBody.append(p);
+                        card.append(img);
+                        card.append(cardBody);
+                        tr.append(card);
+                    }
+                }
+            }
 
         });
-    });
-});
 
-$(function () {
-    $('#goBtn').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 800);
-                return false;
-            }
-        }
     });
+
+    // Smooth Scrolling Functionality
+    $(function () {
+        $('#goBtn').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 800);
+                    return false;
+                }
+            }
+        });
+    });
+
 });
